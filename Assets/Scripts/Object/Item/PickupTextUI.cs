92e8a520196cf7088219642bdbedce8452a103f9
Carlo <93345815+Finn331@users.noreply.github.com>
2026-07-03 22:00:05 +0700

    Add pickup text float-up animation

--- Assets/Scripts/Object/Item/PickupTextUI.cs
+++ Assets/Scripts/Object/Item/PickupTextUI.cs
@@ -5,19 +5,56 @@ public class PickupTextUI : MonoBehaviour
 {
     public TextMeshProUGUI text;
     public float lifeTime = 1f;
+    public float floatDistance = 80f;
+    public LeanTweenType easeType = LeanTweenType.easeOutQuad;
 
-    void Start()
+    private void Start()
     {
-        Destroy(gameObject, lifeTime);
+        if (text == null)
+        {
+            text = GetComponent<TextMeshProUGUI>();
+        }
+
+        RectTransform rect = GetComponent<RectTransform>();
+        if (rect != null)
+        {
+            LeanTween.moveY(rect, rect.anchoredPosition.y + floatDistance, lifeTime)
+                .setEase(easeType);
+        }
+
+        if (text != null)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 1f);
+            LeanTween.value(gameObject, 1f, 0f, lifeTime)
+                .setEase(LeanTweenType.easeInQuad)
+                .setOnUpdate((float val) =>
+                {
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, val);
+                })
+                .setOnComplete(() =>
+                {
+                    Destroy(gameObject);
+                });
+        }
+        else
+        {
+            Destroy(gameObject, lifeTime);
+        }
     }
 
     public void Setup(string itemName, int amount)
     {
-        text.text = itemName + " +" + amount;
+        if (text != null)
+        {
+            text.text = itemName + " +" + amount;
+        }
     }
 
     public void SetupMessage(string message)
     {
-        text.text = message;
+        if (text != null)
+        {
+            text.text = message;
+        }
     }
 }
